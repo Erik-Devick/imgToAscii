762918d15245456a22bcae24583b5ece4534af5f
Erik-Devick <erikdevick1@gmail.com>
2024-11-22 13:39:24 -0600

    commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
-img = Image.open("lisa.png")
+filename = "derick"
+
+img = Image.open(f"{filename}.png")
 width,height = img.size
 pxlst = list(img.getdata())
 
@@ -26,28 +28,28 @@ for rgbrow in pixels:
     brightMatrix.append(brightrow)
     brightrow = []
 
-with open ("out.txt","w") as out:
+with open (f"{filename}.txt","w") as out:
     for r in brightMatrix:
         for val in r:
             if (0<= val) and (val <=25.5):
-                print(".",end="",file=out)
+                print(". ",end="",file=out)
             elif (25.5 < val) and (val <= 51):
-                print(",",end="",file=out)
+                print(", ",end="",file=out)
             elif (51.0 <= val) and (val < 76.5):
-                print(":",end="",file=out)
+                print(": ",end="",file=out)
             elif (76.5 <= val) and (val < 102.0):
-                print("-",end="",file=out)
+                print("- ",end="",file=out)
             elif (102.0 <= val) and (val < 127.5):
-                print("=",end="",file=out)
+                print("= ",end="",file=out)
             elif (127.5 <= val) and (val < 153.0):
-                print("+",end="",file=out)
+                print("+ ",end="",file=out)
             elif (153.0 <= val) and (val < 178.5):
-                print("*",end="",file=out)
+                print("* ",end="",file=out)
             elif (178.5 <= val) and (val < 204.0):
-                print("#",end="",file=out)
+                print("# ",end="",file=out)
             elif (204.0 <= val) and (val < 229.5):
-                print("%",end="",file=out)
+                print("% ",end="",file=out)
             elif (229.5 <= val) and (val < 255.0):
-                print("@",end="",file=out)
+                print("@ ",end="",file=out)
         print("",file=out)
     
